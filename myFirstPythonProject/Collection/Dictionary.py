b2ad2715436c70d36a4105ba7e034fcd313084c5
SIDDHANT-NISHANT <SIDDHANT-NISHANT@LENOVO-LAPTOP>
2021-04-15 21:12:21 +0530

    Python code commit

---
+++ myFirstPythonProject/Collection/Dictionary.py
@@ -0,0 +1,21 @@
+"""
+myDict ={1:'sid',2:'harish',3:'vishesh'} 
+print(myDict[1])
+print(myDict.get(3,'Not present'))
+keys=[1,2,3]
+values=['banana','apple','orange']
+mergedDict = dict(zip(keys,values))
+print(mergedDict)
+myDict[4]='nishant'
+print(myDict)
+del myDict[2]
+print(myDict)
+print(myDict.values())
+
+djaskdjasdds
+dsadasddasdasd
+"""
+x=y=z="dasd"
+cv=5
+cvsd=double(5)
+print(x,y,z,cvsd)
